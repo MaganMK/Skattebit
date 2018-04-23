a024d0ebc87d8ff8ac781b3934f8ba668e07cc74
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2018-04-23 18:19:30 +0200

    fjerne

--- scripts/Exchanges/Generic.js
+++ scripts/Exchanges/Generic.js
@@ -1,7 +1,5 @@
 import {Transaction} from "../Transaction.js";
 
-var loading = document.getElementById("loadingWrapper");
-
 //Må inneholde Tidspunkt (dd/mm/yyyy evt hh:mm:ss)	Valuta	Mengde	Type	Marked
 export function saveGenericTransaction(data)
 {
